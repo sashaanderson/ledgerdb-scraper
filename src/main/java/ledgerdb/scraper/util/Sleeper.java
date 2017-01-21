d63fa47f5e83ff33fe763ff8bde62129e4802b41
U-FLASH\alex <sashaanderson@users.noreply.github.com>
2017-01-21 12:05:14 -0500

    Sleeper

---
+++ src/main/java/ledgerdb/scraper/util/Sleeper.java
@@ -0,0 +1,17 @@
+package ledgerdb.scraper.util;
+
+import java.util.concurrent.TimeUnit;
+
+public class Sleeper {
+
+    public void sleepBetween(int minUnit, int maxUnit, TimeUnit unit) {
+        long minTime = TimeUnit.MILLISECONDS.convert(minUnit, unit);
+        long maxTime = TimeUnit.MILLISECONDS.convert(minUnit, unit);
+        long time = minTime + (long)(Math.random() * (maxTime - minTime));
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
